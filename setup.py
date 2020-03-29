2e0247ffc6d04b7486ae0cc91a04e3533917e92b
Michel Courtine <github@michak.net>
2020-03-29 18:27:24 +0200

    updated setup.py
    Signed-off-by: Michel Courtine <github@michak.net>

--- setup.py
+++ setup.py
@@ -42,12 +42,12 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1',  # Required
+    version='0.1.0',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#summary
-    description='t is for terminal',  # Optional
+    description='t is for terminal. iTerm2 automated configuration',  # Optional
 
     # This is an optional longer description of your project that represents
     # the body of text which users will see when they visit PyPI.
